8d31e28e9cab5259a3e9d5df9694b76856487dc8
Ulrich Barrot <ulrich@barrot.dev>
2022-02-12 22:22:24 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # fritzdemo
-Minimalistic example for changing SSID and password on Fritzboxes and repeaters with Fritzconnection
+Minimalistic example for changing SSID and password on avm routers and repeaters with FritzConnection.
+
+As a base dependency you have to install fritzconnection via
+
+```bash
+pip3 install fritzconnection
+```
+
+After that just create a connection to your FRITZ!box and every other fritz device that should be updated:
+```python
+# 
+FritzConnection(address='192.168.178.1', user='fritz****', password='<password>', use_tls=False)
+```
+Please inspect the set_password_and_ssid function in set_ssid_password.py to see how to receive and set the ssid and password of your network. The demo defaults to the guest wifi config "WLANConfiguration:3". You can change this to your needs. 
+
+The example codes generates the phrase "It's dayOtTheWeek \(^_^)/" in german with a specific emoji for every day of the week. Not really useful, just a little easteregg.
---
+++ set_ssid_password.py
@@ -0,0 +1,41 @@
+from fritzconnection import FritzConnection
+import locale
+import datetime
+
+locale.setlocale(locale.LC_ALL, "de_DE.UTF-8")
+
+emojis = {
+    "Montag": "(*__*)",
+    "Dienstag": "(@__*)",
+    "Mittwoch": "(@_@)",
+    "Donnerstag": "\(@_@)",
+    "Freitag": "\(@_@)/",
+    "Samstag": "\(^_^)/",
+    "Sonntag": "(*_@)"
+}
+
+
+def set_password_and_ssid(f_connection, new_ssid, password, wifi_config='WLANConfiguration:3'):
+    print("Setting ssid and password on {}".format(f_connection.address))
+    security_keys = f_connection.call_action(wifi_config, 'GetSecurityKeys')
+    security_keys['NewKeyPassphrase'] = password
+    f_connection.call_action(wifi_config,
+                             'SetSecurityKeys',
+                             arguments=security_keys)
+    ssid = f_connection.call_action(wifi_config, 'GetSSID')
+    ssid['NewSSID'] = new_ssid
+    f_connection.call_action(wifi_config,
+                             'SetSSID',
+                             arguments=ssid)
+
+
+day = datetime.datetime.now().strftime("%A")
+guest_ssid = 'Heute ist ' + day + ' ' + emojis[day]
+guest_password = "<mypassword>"
+
+devices = [
+    FritzConnection(address='192.168.178.1', user='<username>', password='<password>', use_tls=False)
+]
+
+for device in devices:
+    set_password_and_ssid(device, guest_ssid, guest_password)
